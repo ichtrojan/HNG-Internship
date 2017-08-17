8499e4d85d47104218a732d9947c069c0331a0de
Michael Okoh <ordrizzy@gmail.com>
2017-08-17 23:57:56 +0100

    Created the index file

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+   $dbhost = 'localhost';
+   $dbuser = 'root';
+   $dbpass = '';
+
+   $conn = mysql_connect($dbhost, $dbuser, $dbpass);
+
+   if(! $conn ) {
+      die('Could not connect: ' . mysql_error());
+   }
+
+   $sql = 'SELECT id, name, age FROM human';
+   mysql_select_db('HNG');
+   $retval = mysql_query( $sql, $conn );
+
+   if(! $retval ) {
+      die('Could not get data: ' . mysql_error());
+   }
+
+   while($row = mysql_fetch_array($retval, MYSQL_ASSOC)) {
+      echo "ID :{$row['id']}  <br> ".
+         "NAME : {$row['name']} <br> ".
+         "AGE : {$row['age']} <br> ".
+         "--------------------------------<br>";
+   }
+
+   echo "Fetched data successfully\n";
+
+   mysql_close($conn);
+?>
